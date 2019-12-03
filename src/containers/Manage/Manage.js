b764ecf3aa452684014570820e3184d80b2898d1
Kelly Crowther <crowther.kelly1@gmail.com>
2019-12-03 12:47:54 -0800

    add editable cell to table

--- src/containers/Manage/Manage.js
+++ src/containers/Manage/Manage.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
-import { Upload, message, Button, Icon, Table } from "antd";
+import { Upload, message, Button, Icon, Table, Form, Select } from "antd";
 
 import parse from "csv-parse";
+
+const { Option } = Select;
 
 function readCSV(info) {
   const csvData = [];
@@ -47,9 +49,111 @@ const columns = [
   },
   {
     title: "Round 1 Result",
-    dataIndex: "Round1Result"
+    dataIndex: "Round1Result",
+    editable: true
   }
 ];
+
+const EditableContext = React.createContext();
+
+const EditableRow = ({ form, index, ...props }) => (
+  <EditableContext.Provider value={form}>
+    <tr {...props} />
+  </EditableContext.Provider>
+);
+
+const EditableFormRow = Form.create()(EditableRow);
+
+class EditableCell extends React.Component {
+  state = {
+    editing: false
+  };
+
+  toggleEdit = () => {
+    console.info("STATE: ", this.state);
+    const editing = !this.state.editing;
+    this.setState({ editing }, () => {
+      if (editing) {
+        this.input.focus();
+      }
+    });
+  };
+
+  save = e => {
+    const { record, handleSave } = this.props;
+    this.form.validateFields((error, values) => {
+      if (error && error[e.currentTarget.id]) {
+        return;
+      }
+      this.toggleEdit();
+      handleSave({ ...record, ...values });
+    });
+  };
+
+  renderCell = form => {
+    this.form = form;
+    const { children, dataIndex, record, title } = this.props;
+    const { editing } = this.state;
+    return editing ? (
+      <Form.Item style={{ margin: 0 }}>
+        {form.getFieldDecorator(dataIndex, {
+          rules: [
+            {
+              required: true,
+              message: `${title} is required.`
+            }
+          ],
+          initialValue: record[dataIndex]
+        })(
+          <Select
+            ref={node => (this.input = node)}
+            onChange={this.save}
+            onBlur={this.save}
+            style={{ width: 120 }}
+          >
+            <Option value={1}>1</Option>
+            <Option value={999}>999</Option>
+            <Option value={3}>3</Option>
+            <Option value={4}>4</Option>
+            <Option value={5}>5</Option>
+            <Option value={6}>6</Option>
+          </Select>
+        )}
+      </Form.Item>
+    ) : (
+      <div
+        className="editable-cell-value-wrap"
+        style={{ paddingRight: 24 }}
+        onClick={this.toggleEdit}
+      >
+        &nbsp;
+        {children}
+      </div>
+    );
+  };
+
+  render() {
+    const {
+      editable,
+      dataIndex,
+      title,
+      record,
+      index,
+      handleSave,
+      children,
+      ...restProps
+    } = this.props;
+    return (
+      <td {...restProps}>
+        {editable ? (
+          <EditableContext.Consumer>{this.renderCell}</EditableContext.Consumer>
+        ) : (
+          children
+        )}
+      </td>
+    );
+  }
+}
 
 export function Manage() {
   const [racers, setRacers] = useState([]);
@@ -75,6 +179,41 @@ export function Manage() {
     }
   };
 
+  const handleSave = row => {
+    const editRacers = racers;
+    const index = editRacers.findIndex(item => row.key === item.key);
+    const item = editRacers[index];
+    editRacers.splice(index, 1, {
+      ...item,
+      ...row
+    });
+    console.info("EDIT RACERS: ", editRacers);
+    setRacers(editRacers);
+  };
+
+  const columnsEditable = columns.map(col => {
+    if (!col.editable) {
+      return col;
+    }
+    return {
+      ...col,
+      onCell: record => ({
+        record,
+        editable: col.editable,
+        dataIndex: col.dataIndex,
+        title: col.title,
+        handleSave: handleSave
+      })
+    };
+  });
+
+  const components = {
+    body: {
+      row: EditableFormRow,
+      cell: EditableCell
+    }
+  };
+
   console.info("RACERS: ", racers);
 
   return (
@@ -87,7 +226,13 @@ export function Manage() {
         </Button>
       </Upload>
 
-      <Table columns={columns} dataSource={racers} rowKey="Place" />
+      <Table
+        components={components}
+        rowClassName={() => "editable-row"}
+        columns={columnsEditable}
+        dataSource={racers}
+        rowKey="Place"
+      />
     </div>
   );
 }
