f9d3e1ba862010db4794b2fdf1bbece13b6ab25e
Kelly Crowther <crowther.kelly1@gmail.com>
2019-12-02 22:35:18 -0800

    create initial routes and import antd styles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Menu, Icon } from "antd";
 
-function App() {
+export default function BasicExample() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <Menu mode="horizontal">
+        <Menu.Item key="home">
+          <Icon type="home" />
+          Home
+          <Link to="/"></Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Icon type="database" />
+          Manage
+          <Link to="/manage"></Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Icon type="dashboard" />
+          Results
+          <Link to="/results"></Link>
+        </Menu.Item>
+      </Menu>
+      <div>
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/manage">
+            <Manage />
+          </Route>
+          <Route path="/results">
+            <Results />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+// You can think of these components as "pages"
+// in your app.
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
     </div>
   );
 }
 
-export default App;
+function Manage() {
+  return (
+    <div>
+      <h2>Manage</h2>
+    </div>
+  );
+}
+
+function Results() {
+  return (
+    <div>
+      <h2>Results</h2>
+    </div>
+  );
+}
