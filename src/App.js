60ce7067486641cfad372c1d94a806c96b1ae4fb
Kelly Crowther <crowther.kelly1@gmail.com>
2020-02-25 13:06:23 -0800

    get sider navigation working and abstracted to json

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Breadcrumb, Layout, Spin, Menu, Icon } from "antd";
 import "./App.css";
 
@@ -16,6 +16,44 @@ import { Subscription } from "@apollo/react-components";
 
 const { SubMenu } = Menu;
 const { Content, Footer, Sider } = Layout;
+
+const routes = [
+  {
+    app: "home",
+    name: "Home",
+    icon: "home",
+    path: "/",
+    subMenu: [],
+    key: "sub3"
+  },
+  {
+    app: "stateQualifiers",
+    name: "State Qualifiers",
+    icon: "rocket",
+    path: "/state-qualifiers",
+    key: "sub1",
+    subMenu: [
+      {
+        name: "Qualifiers",
+        path: "/state-qualifiers",
+        key: "1"
+      },
+      {
+        name: "Borderline",
+        path: "/borderline",
+        key: "2"
+      }
+    ]
+  },
+  {
+    app: "kingsCourt",
+    name: "Kings Court",
+    icon: "crown",
+    path: "/kings-court",
+    subMenu: [],
+    key: "sub2"
+  }
+];
 
 const App = () => {
   const { loading } = useAuth0();
@@ -42,22 +80,34 @@ const App = () => {
                 defaultOpenKeys={["sub1"]}
                 style={{ height: "100%", borderRight: 0 }}
               >
-                <SubMenu
-                  key="sub1"
-                  title={
-                    <span>
-                      <Icon type="rocket" />
-                      State Qualifiers
-                    </span>
-                  }
-                >
-                  <Menu.Item key="1">Qualifiers</Menu.Item>
-                  <Menu.Item key="2">Borderline</Menu.Item>
-                </SubMenu>
-                <Menu.Item key="sub2">
-                  <Icon type="crown" />
-                  <span>Kings Court</span>
-                </Menu.Item>
+                {routes.map(route => {
+                  return route.subMenu.length ? (
+                    <SubMenu
+                      key={route.key}
+                      title={
+                        <span>
+                          <Icon type={route.icon} />
+                          {route.name}
+                        </span>
+                      }
+                    >
+                      {route.subMenu.map(sub => {
+                        return (
+                          <Menu.Item key={sub.key}>
+                            {sub.name}
+                            <Link to={sub.path}></Link>
+                          </Menu.Item>
+                        );
+                      })}
+                    </SubMenu>
+                  ) : (
+                    <Menu.Item key={route.key}>
+                      <Icon type={route.icon} />
+                      <span>{route.name}</span>
+                      <Link to={route.path}></Link>
+                    </Menu.Item>
+                  );
+                })}
               </Menu>
             </Sider>
             <Layout style={{ padding: "0 24px 24px" }}>
@@ -73,6 +123,12 @@ const App = () => {
                   <Switch>
                     <Route exact path="/">
                       <Home />
+                    </Route>
+                    <Route exact path="/state-qualifiers">
+                      <StateQualifiersHome />
+                    </Route>
+                    <Route exact path="/kings-court">
+                      <KingsCourtHome />
                     </Route>
                     <PrivateRoute path="/manage" component={Manage} />
                     <Route path="/results">
@@ -95,6 +151,30 @@ const App = () => {
 };
 
 function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      <p>
+        Welcome to the OISRA Nordic management tool. Choose an application to
+        run on the left.
+      </p>
+    </div>
+  );
+}
+
+function StateQualifiersHome() {
+  return (
+    <div>
+      <h2>State Qualifiers</h2>
+      <p>
+        Welcome to the State Qualifiers tool. Simply upload your formatted
+        results to calculate the state qualifiers.
+      </p>
+    </div>
+  );
+}
+
+function KingsCourtHome() {
   const ADD_BATTLESTAR_SUBSCRIPTION = `
     subscription AddBattleStar {
       addBattleStar {
@@ -104,7 +184,7 @@ function Home() {
   `;
   return (
     <div>
-      <h2>Home</h2>
+      <h2>Kings Court</h2>
       <p>
         Head over to the Manage page for the magic.{" "}
         <span role="img" aria-label="sparkles">
